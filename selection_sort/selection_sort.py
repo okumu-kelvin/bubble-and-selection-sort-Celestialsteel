7bd68d0eaeb74045d7f5044a79145389b3eab187
Celestial Renegade <llime6260@gmail.com>
2025-06-20 14:46:44 +0300

    This is a submission

--- selection_sort/selection_sort.py
+++ selection_sort/selection_sort.py
@@ -21,11 +21,12 @@ def main():
         print("The list is empty.")
     sorted_list = selection_sort(unsorted_list)
     print("Sorted list is:", sorted_list)
-
+x
 
 if __name__ == "__main__":
     main()
 
+#this is a comment
 
     # TODO: Implement selection sort
     pass
