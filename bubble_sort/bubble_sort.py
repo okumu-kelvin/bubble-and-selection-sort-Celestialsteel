bdc5d0ed7d70ee84b5484c5865802b4b402669d6
Celestial Renegade <llime6260@gmail.com>
2025-06-20 13:25:11 +0300

    Bubble sort and selection sort implementation

--- bubble_sort/bubble_sort.py
+++ bubble_sort/bubble_sort.py
@@ -1,3 +1,23 @@
 def bubble_sort(unsorted_list):
-    # TODO: Implement bubble sort
-    pass
+    n = len(unsorted_list)
+    for i in range(n):
+        for j in range(0, n - i - 1):
+            if unsorted_list[j] > unsorted_list[j + 1]:
+                unsorted_list[j], unsorted_list[j + 1] = unsorted_list[j + 1], unsorted_list[j]
+    return unsorted_list
+
+
+def main():
+    print("Enter a list of numbers separated by spaces: ")
+    unsorted_list = list(map(int, input().split()))    
+    if not unsorted_list:
+        print("The list is empty.")
+    sorted_list = bubble_sort(unsorted_list)
+    print("Sorted list is:", sorted_list)
+        
+
+if __name__ == "__main__":
+    main()        
+
+    sorted_list = bubble_sort(unsorted_list)
+    print("Sorted list is:", sorted_list)
--- selection_sort/selection_sort.py
+++ selection_sort/selection_sort.py
@@ -1,3 +1,31 @@
 def selection_sort(arr):
+    n = len(arr)
+    
+    for i in range(n):
+        min_index = i
+
+        for j in range(i + 1, n):
+            if arr[j] < arr[min_index]:
+                min_index = j
+
+        arr[i], arr[min_index] = arr[min_index], arr[i]
+
+    return arr
+
+
+
+def main():
+    print("Enter a list of numbers separated by spaces: ")  
+    unsorted_list = list(map(int, input().split()))
+    if not unsorted_list:
+        print("The list is empty.")
+    sorted_list = selection_sort(unsorted_list)
+    print("Sorted list is:", sorted_list)
+
+
+if __name__ == "__main__":
+    main()
+
+
     # TODO: Implement selection sort
     pass
